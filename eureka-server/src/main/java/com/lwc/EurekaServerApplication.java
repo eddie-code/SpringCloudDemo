f55b0d26627cdbe656a4c7f3526ec2837e22f3cf
eddie-code <231071372@qq.com>
2018-03-26 17:07:55 +0800

    服务注册中心

---
+++ eureka-server/src/main/java/com/lwc/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.lwc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Eddie
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
