93c11dab9987294ef7b6d6afea0986eae6d412e8
eddie-code <231071372@qq.com>
2018-03-27 11:24:18 +0800

    Revert "first commit"
    This reverts commit ce01a6647e472cbf10b80ce305edf61b615d1357.
    
    # Conflicts:
    #	README.md

---
+++ eureka-feign/src/main/java/com/lwc/controller/FeignController.java
@@ -0,0 +1,12 @@
+package com.lwc.controller;
+
+/**
+ * @author eddie.lee
+ * @Package com.lwc.controller
+ * @ClassName FeignController
+ * @description
+ * @date created in 2018-03-27 10:50
+ * @modified by
+ */
+public class FeignController {
+}
---
+++ eureka-feign/src/main/java/com/lwc/service/FeignClientService.java
@@ -0,0 +1,12 @@
+package com.lwc.service;
+
+/**
+ * @author eddie.lee
+ * @Package com.lwc.service
+ * @ClassName FeignClientService
+ * @description
+ * @date created in 2018-03-27 10:43
+ * @modified by
+ */
+public interface FeignClientService {
+}
