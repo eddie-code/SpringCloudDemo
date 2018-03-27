c2869e61ae73cdc4cdf2f4306198c9f1468f6e17
eddie-code <231071372@qq.com>
2018-03-27 11:26:22 +0800

    服务消费者（Feign）

--- eureka-feign/src/main/java/com/lwc/controller/FeignController.java
+++ eureka-feign/src/main/java/com/lwc/controller/FeignController.java
@@ -1,12 +1,30 @@
 package com.lwc.controller;
 
+import com.lwc.service.FeignClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author eddie.lee
  * @Package com.lwc.controller
  * @ClassName FeignController
- * @description
+ * @description 服务消费者（Feign）
  * @date created in 2018-03-27 10:50
  * @modified by
  */
+@RestController
+@RequestMapping("/eureka")
 public class FeignController {
+
+    @Autowired
+    private FeignClientService feignClientService;
+
+    @GetMapping("/feign")
+    public String feign(@RequestParam("name") String name) {
+        return feignClientService.feignToClientByName(name);
+    }
+
 }
--- eureka-feign/src/main/java/com/lwc/service/FeignClientService.java
+++ eureka-feign/src/main/java/com/lwc/service/FeignClientService.java
@@ -1,5 +1,9 @@
 package com.lwc.service;
 
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
  * @author eddie.lee
  * @Package com.lwc.service
@@ -8,5 +12,10 @@ package com.lwc.service;
  * @date created in 2018-03-27 10:43
  * @modified by
  */
+@FeignClient("eureka-client")
 public interface FeignClientService {
+
+    @GetMapping("/eureka/client")
+    String feignToClientByName(@RequestParam("name") String name);
+
 }
