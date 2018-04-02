46a55c4b8883e1a6b3bb0e4d2c457b240fadd62f
eddie-code <231071372@qq.com>
2018-04-02 22:13:11 +0800

    路由网关(zuul)

---
+++ eureka-zuul/src/main/java/com/lwc/EurekaZuulApplication.java
@@ -0,0 +1,19 @@
+package com.lwc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author Eddie
+ */
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaZuulApplication.class, args);
+    }
+}
