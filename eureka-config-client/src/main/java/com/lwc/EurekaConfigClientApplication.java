a4907646c20fb1965da8a7f22627d8991f49d61d
eddie-code <231071372@qq.com>
2018-04-05 20:48:59 +0800

    高可用的分布式配置中心(Spring Cloud Config)

--- eureka-config-client/src/main/java/com/lwc/EurekaConfigClientApplication.java
+++ eureka-config-client/src/main/java/com/lwc/EurekaConfigClientApplication.java
@@ -2,10 +2,14 @@ package com.lwc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Eddie
  */
+@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class EurekaConfigClientApplication {
 
--- eureka-config-server/src/main/java/com/lwc/EurekaConfigServerApplication.java
+++ eureka-config-server/src/main/java/com/lwc/EurekaConfigServerApplication.java
@@ -2,13 +2,14 @@ package com.lwc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Eddie
  */
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class EurekaConfigServerApplication {
 
