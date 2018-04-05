aff4f4713d20fd71902b1c50e2b6c0673271d193
eddie-code <231071372@qq.com>
2018-04-05 21:09:44 +0800

    高可用的分布式配置中心(Spring Cloud Config)

---
+++ eureka-config-client-cluster/src/main/java/com/lwc/EurekaConfigClientClusterApplication.java
@@ -0,0 +1,12 @@
+package com.lwc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaConfigClientClusterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConfigClientClusterApplication.class, args);
+    }
+}
